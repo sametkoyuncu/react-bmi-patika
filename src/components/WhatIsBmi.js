61393d727c1953f33b3b8b3a0acec0d66966ec0c
sametkoyuncu <sametkoyuncu@live.com>
2022-01-02 23:16:31 +0300

    Add what is bmi page

---
+++ src/components/WhatIsBmi.js
@@ -0,0 +1,23 @@
+function WhatIsBmi() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Vücut Kitle İndeksi Nedir?</h1>
+      <p className="text-md text-gray-800 mt-3">
+        Vücut kitle indeksi; ağırlığın boyun karesine bölünmesi formülü ile
+        hesaplanmaktadır. Burada ağırlık kilogram ve boy da metre cinsinden
+        alınmaktadır. VKİ olarak kısaltılmaktadır. Body mass index olarak da
+        bilinmektedir ve BMI da denmektedir. Bireyin ideal kilosunu görebilmesi
+        ve hangi ağırlık kategorisinde olduğunu öğrenebilmesi açısından
+        kullanılan bir formüldür. Ancak vücut yağı ve kas kütlesi için detaylı
+        bir bilgi vermediğinden, özellikle vücut geliştirme ile ilgilenenler
+        için tatmin edici bir sonuç vermeyebilmektedir. Çünkü aşırı kas
+        kütlesine sahip olduklarından sadece boy ve ağırlık ile yapılan
+        hesaplama sonucunda BMI değerleri yüksek çıkabilmektedir. Ayrıca kas
+        kütlesini kaybetmiş olan yaşlı bir bireyin yağ oranının fazla olduğu da
+        vücut kitle indeksi ile görülememektedir.
+      </p>
+    </div>
+  )
+}
+
+export default WhatIsBmi
